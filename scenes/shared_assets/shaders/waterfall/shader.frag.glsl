a888a96d38cddb0eb8f4ff9204242bf22a59cfd0
o-Eulalie-o <55194632+o-Eulalie-o@users.noreply.github.com>
2020-05-28 18:36:21 +0200

    Add files via upload

--- scenes/shared_assets/shaders/waterfall/shader.frag.glsl
+++ scenes/shared_assets/shaders/waterfall/shader.frag.glsl
@@ -12,27 +12,20 @@ uniform float time;
 uniform sampler2D falaise;
 uniform sampler2D texture_noise;
 
-
-
 out vec4 FragColor;
 
 
-// https://www.shadertoy.com/view/XdS3RW
-vec3 screen_blend(vec3 s, vec3 d) {
-	return s+d - s*d;
-}
-
 float get_mask(vec2 uv) {
-    //uv.x *= iResolution.x / iResolution.y; (jouer sur le flottant)
+    uv.x *= 5.f;
     uv.x += sign(uv.x)*uv.y*.2;
     uv.x = abs(uv.x);
-	return clamp(1.-(smoothstep(0.2, .6, uv.x)), 0., 1.);
+        return clamp((1.-(smoothstep(0.2, .6, uv.x))), 0., 1.);
 }
 
 // https://catlikecoding.com/unity/tutorials/flow/texture-distortion/
 vec2 flow(vec2 uv, vec2 flowmap, float phase, float t, out float weight) {
 	float progress = fract(t + phase);
-	vec2 displacement = flowmap * progress;
+        vec2 displacement = 0.1f*flowmap * progress;
     weight = 1. - abs(progress*2.-1.);
 	return uv + displacement;
 }
@@ -46,22 +39,22 @@ float fbm(sampler2D sampler, vec2 p) {
 	// Loop of octaves
 	for (int i = 0; i < 5; i++) {
         value += amplitude * (texture(sampler, p*frequency).r);
-		frequency *= 2.;
-		amplitude *= .5;
+        frequency *= 2.;
+        amplitude *= .5;
 	}
-	return value;
+        return value;
 }
 
 
 void main()
 {
     vec2 uv = vec2(fragment.texture_uv.y, fragment.texture_uv.x);
-    uv.x = uv.x*2.-1.5;// jouer sur la translation ici pour décaler la facade
+    uv.x = uv.x*2.-1.55;// jouer sur la translation ici pour décaler la facade
     
     vec2 flowuv = uv*.03 + vec2(0., time*0.01);
     float noise = fbm(texture_noise, flowuv);
     vec2 flowmap = vec2(0., smoothstep(0.2, 1.,noise)) * .006;
-	float weightA, weightB;
+    float weightA, weightB;
     float t = time * .8;
     vec2 uvA = flow(flowuv, flowmap, 0.0, t, weightA);
     vec2 uvB = flow(flowuv, flowmap, 0.5, t, weightB);
@@ -72,17 +65,13 @@ void main()
     float waterfall_mask = get_mask(uv);
     float spray_mask = 1.-length(vec2(uv.x*.8, pow(uv.y, .5))*1.7);
     uv.y += .5;
-    vec2 radial_uv = 0.01*vec2(atan(uv.x, uv.y), length(uv)) + vec2(0., -time*0.002);
-    float spray = fbm(texture_noise, radial_uv);
-
-    vec3 background = texture(falaise, uv).rgb;
     
     vec3 blue = vec3(0.6, .6, .9);
-    vec3 waterfall = ((1.-flow)*blue + smoothstep(0., 1.,flow)) * waterfall_mask;
-    vec3 col = mix(background, waterfall, waterfall_mask);
-    //col += vec3(spray_mask*spray*(1.-waterfall_mask));
-    spray_mask = clamp(spray_mask, 0., 1.);
-    //col = screen_blend(vec3(spray*spray_mask*2.5), col);
+    vec3 col = ((1.-flow)*blue + smoothstep(0., 1.,flow)) *waterfall_mask;
+    float color_alpha = 0.0f;
+    if (col.x + col.y + col.z > 1.5f){
+        color_alpha = col.x + col.y + col.z;
+    }
 
-    FragColor = vec4(col,1.0);
+    FragColor = vec4(col, color_alpha);
 }
